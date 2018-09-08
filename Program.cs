58c06a63c0e34326063f1f3a85f3569b0cba8b4f
David Worthington <wortho@users.noreply.github.com>
2018-09-08 19:26:33 +0200

    Adds the initial files for the project.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace NAVWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length < 2)
+            {
+                Console.WriteLine("Please provide username and password.");
+                return;
+            }
+            var username = args[0];
+            var password = args[1];
+            var client = new Services.ReportService_PortClient();
+            var binding = client.Endpoint.Binding as BasicHttpBinding;
+            binding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+            client.ClientCredentials.UserName.UserName = username;
+            client.ClientCredentials.UserName.Password = password;
+            for (int i = 0; i < 10; i++)
+            {
+                var result = client.SaveToPDFAsync(311).Result;
+                Console.WriteLine($"Result:{result.return_value}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # navwebserviceclient
-Dynamic NAV Web Service Client using dotnet core
+Dynamic NAV Web Service Client using dotnet core.
+The example calls a NAV Codeunit 50079 exposed as a web service that runs a report and saves it as a PDF on the Server.
+To create the web service:
+1. Import the file COD50079.txt.
+2. Publish the CodeUnit as a web service using the WebServices page in the NAV Client or using PowerShell.
+
+To generate a service reference:
+1. Download the WSDL from the NAV Web Service endpoint.
+2. Run the command `dotnet svcutil Service.wsdl --namespace "*,NAVWS.Services"`
+
+To run the app you need to provide the NavUserName credentials on the command line. 
+
